00f71029846de818e2bacc0752feafcf7c84994e
dan-stachlewski <dan.stachlewski@gmail.com>
2018-03-23 08:31:56 +1100

    Created the Display Activity: - extended the Display Activity, - added the onCreate Method from Main Activity, - added Display Activity to the Manifest File, Added the button on Main Activity & text to be displayed on the Display Activity.

---
+++ app/src/main/java/com/example/dnafv/medalarm/Display.java
@@ -0,0 +1,15 @@
+package com.example.dnafv.medalarm;
+
+import android.os.Bundle;
+
+/**
+ * Created by dnafv on 23/03/2018.
+ */
+
+public class Display extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+    }
+}
