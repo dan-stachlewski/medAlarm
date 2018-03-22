8999c140a061c75ea395eced087974615011c0ac
dan-stachlewski <dan.stachlewski@gmail.com>
2018-03-23 08:38:13 +1100

    Created the Method for the button to Activate the Display Activity when the user clicks on it. The Display Activity opens and a message is displayed on the desktop. The app works in the emulator.

--- app/src/main/java/com/example/dnafv/medalarm/MainActivity.java
+++ app/src/main/java/com/example/dnafv/medalarm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.dnafv.medalarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Method that will open the Display Activity when Clicked.
+    public void onButtonClick(View v){
+        if(v.getId() == R.id.displayBtn){
+            Intent i = new Intent(MainActivity.this, Display.class);
+            startActivity(i);
+        }
     }
 }
