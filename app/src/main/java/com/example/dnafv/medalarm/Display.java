b1ea584eef23646862570e4f7f69c73cabb9aa65
dan-stachlewski <dan.stachlewski@gmail.com>
2018-03-23 11:56:05 +1100

    Created a Login Form, re-purposing the button to log the user in. When the user pressed login the Display Activity is activated and the username entered is transferred to the textfield in the Display Activty.

--- app/src/main/java/com/example/dnafv/medalarm/Display.java
+++ app/src/main/java/com/example/dnafv/medalarm/Display.java
@@ -1,6 +1,7 @@
 package com.example.dnafv.medalarm;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by dnafv on 23/03/2018.
@@ -11,5 +12,9 @@ public class Display extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display);
+        String username = getIntent().getStringExtra("Username");
+
+        TextView tv = (TextView)findViewById(R.id.textViewUsername);
+        tv.setText(username);
     }
 }
--- app/src/main/java/com/example/dnafv/medalarm/MainActivity.java
+++ app/src/main/java/com/example/dnafv/medalarm/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +14,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Method that will open the Display Activity when Clicked.
+    // Method that will Login User when they add their credentials and click login.
+    // It takes 1 View Object & checks if View Object Id matches with Btn Id
+    // It creates 1 Intent Object (when the button is clicked) starting the New Activity using the onCreate Method
     public void onButtonClick(View v){
-        if(v.getId() == R.id.displayBtn){
+        if(v.getId() == R.id.logInBtn){
+
+            // This is how we fetch the Username
+            EditText a = (EditText)findViewById(R.id.textFieldUsername);
+            String str = a.getText().toString();
+
             Intent i = new Intent(MainActivity.this, Display.class);
+            //Transfer Username from this Activity to DIsplay Activity
+            i.putExtra("Username",str);
             startActivity(i);
         }
     }
